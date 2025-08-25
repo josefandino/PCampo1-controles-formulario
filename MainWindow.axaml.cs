6c9b330352ee2eebc39f816535706f6da9e7ab1b
José Fandiño <josefandinod@gmail.com>
2025-08-25 18:58:57 -0500

    First commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PCampo1ControlesFormulario;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
